426f2c3a6b57c0d971c62b4e88427ac19b148d25
GraveGit <olegtsurikhin656123@gmail.com>
2023-07-10 17:06:44 +0300

    Add gitignore

--- Task_1.62/main.cpp
+++ Task_1.62/main.cpp
@@ -1,8 +1,11 @@
+//Дано вещественное число a. Пользуясь только операцией умножения, получить
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!";
+    // a^3 и a^10 за 4 операции
+
+
     return 0;
 }
 
