6d48cf6ca87b4d084c2bd01814602b1c1ed7e4ad
GraveGit <olegtsurikhin656123@gmail.com>
2023-07-14 16:21:53 +0300

    Add Task_8.59 solution

---
+++ Task_8.59/main.cpp
@@ -0,0 +1,29 @@
+//8.59.*Найти все простые несократимые дроби, заключенные между 0 и 1, знамена-
+// тели которых не превышают 7 (дробь задается двумя натуральными числами — числителем и знаменателем).
+
+#include <iostream>
+
+int main( int argc, char* argv[] )
+{
+  int remains = 0;
+
+  for ( int denominator = 1; denominator <= 7; denominator++ ) //Знаменатель
+  {
+    for ( int numerator = 1; numerator < denominator; numerator++ )//Числитель
+    {
+      remains = denominator % numerator;
+
+      if ( numerator == 1 )
+        std::cout << numerator << "/" << denominator << " ";
+      else if ( remains == 1 )
+        std::cout << numerator << "/" << denominator << " ";
+      else if ( remains != 0 )
+      {
+        if ( ( denominator % remains != 0 ) && ( numerator % remains != 0 ) )
+          std::cout << numerator << "/" << denominator << " ";
+      }
+    }
+  }
+
+  return 0;
+}
