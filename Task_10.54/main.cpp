8b48598089316719668f5f433c16beb40526b7dd
GraveGit <olegtsurikhin656123@gmail.com>
2023-07-16 12:27:08 +0300

    Add Task_10.54 solution

---
+++ Task_10.54/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void binaryConvert( int decimalNumber )
+{
+  if ( decimalNumber > 0 )
+  {
+    binaryConvert( decimalNumber / 2 );
+    std::cout << decimalNumber % 2;
+  }
+}
+
+int main( int argc, char* argv[] )
+{
+  int decimalNumber = 0;
+  std::cout << "Enter decimal number: ";
+  std::cin >> decimalNumber;
+
+  binaryConvert( decimalNumber );
+  return 0;
+}
